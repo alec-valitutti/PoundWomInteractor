183952a52a44d395363792b8ba7184d537b415a2
Alec Valitutti <alecvalitutti@gmail.com>
2024-06-18 19:54:19 -0400

    add

--- PoundWomInteractor/PoundWomInteractor.App/Program.cs
+++ PoundWomInteractor/PoundWomInteractor.App/Program.cs
@@ -1,11 +1,18 @@
 ﻿using PoundWomInteractor;
+using System.Threading.Tasks;
 
-Application application = new Application();
-
-await application.Test();
-var clanMembers = await application.GetPoundPlayersByEHP(2000);
-
-foreach (var member in clanMembers)
+internal class Program
 {
-    Console.WriteLine(member.player.displayName);
+    private static async Task Main(string[] args)
+    {
+        Application application = new Application();
+
+        await application.Test();
+        var clanMembers = await application.GetPoundPlayersByEHP(2000);
+
+        foreach (var member in clanMembers)
+        {
+            Console.WriteLine(member.player.displayName);
+        }
+    }
 }
